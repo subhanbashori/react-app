ffb771e40ce59ecfe4cc19f5351a5fa029eb09cf
subhanbashori <subhanbashori11@gmail.com>
2019-06-25 10:16:27 +0700

    Updates

---
+++ precache-manifest.5dd45c3b30994f7f6f49aaa6755aed3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37dda9d7edd649cf502ff4876b7a651",
+    "url": "/subhanbashori/react-app/index.html"
+  },
+  {
+    "revision": "b2882cd6f21f5fbed639",
+    "url": "/subhanbashori/react-app/static/css/main.c44ccba6.chunk.css"
+  },
+  {
+    "revision": "0e2028188eb1c47e0c8c",
+    "url": "/subhanbashori/react-app/static/js/2.a5d31feb.chunk.js"
+  },
+  {
+    "revision": "b2882cd6f21f5fbed639",
+    "url": "/subhanbashori/react-app/static/js/main.dc9233f3.chunk.js"
+  },
+  {
+    "revision": "6bdda74136109d409798",
+    "url": "/subhanbashori/react-app/static/js/runtime~main.f6fb8bb5.js"
+  }
+]);
